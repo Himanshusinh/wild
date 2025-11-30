461fbfa243c5bdd882d57d2d6c1377a305ea46f2
xenitsu889 <rajdeepsinghchavda7@gmail.com>
2025-11-30 17:29:20 +0530

    sd

---
+++ src/components/skeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+
+import React from "react";
+
+
+
+export type SkeletonProps = React.HTMLAttributes<HTMLDivElement>;
+
+export const Skeleton: React.FC<SkeletonProps> = ({ className, ...props }) => {
+
+  return <div className={cn("animate-pulse rounded-md bg-white/10", className)} {...props} />;
+
+};
+
